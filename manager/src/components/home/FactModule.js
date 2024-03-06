992ff2598de3c36ecde9b99cc229ab5373c37198
Austin Duvall <132301084+austin-duvall@users.noreply.github.com>
2024-03-06 11:48:24 -0800

    fast facts logic

--- manager/src/components/home/FactModule.js
+++ manager/src/components/home/FactModule.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function FactModule({ header, content }) {
+const easeOutQuad = t => t * (2 - t);
+const frameDuration = 1000 / 60;
+
+const CountUpAnimation = ({ number, duration = 2000 }) => {
+  const countTo = parseInt(number, 10);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let frame = 0;
+    const totalFrames = Math.round(duration / frameDuration);
+    const counter = setInterval(() => {
+      frame++;
+      const progress = easeOutQuad(frame / totalFrames);
+      setCount(countTo * progress);
+
+      if (frame === totalFrames) {
+        clearInterval(counter);
+      }
+    }, frameDuration);
+
+    return () => clearInterval(counter);
+  }, [number, duration]);
+
+  return <span>{Math.floor(count)}</span>;
+};
+
+export default function FactModule({ header, numberOne, numberTwo, key }) {
   return (
-    <li>
+    <li key={key}>
+      {!numberTwo ? (
+        <p>
+          <CountUpAnimation number={numberOne}></CountUpAnimation>
+        </p>
+      ) : (
+        <p>
+          <CountUpAnimation number={numberOne}></CountUpAnimation> years, <CountUpAnimation number={numberTwo}></CountUpAnimation> months
+        </p>
+      )}
+
       <p>{header}</p>
-      <h2>{content}</h2>
     </li>
   )
 }
--- manager/src/components/home/Home.js
+++ manager/src/components/home/Home.js
@@ -1,30 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFetchEmployees } from '../../hooks/useFetchEmployees';
 import FactModule from './FactModule';
-import { differenceInDays } from 'date-fns/differenceInDays';
+import { differenceInDays } from 'date-fns';
 
 export default function Home() {
+  // Initialize state with values from localStorage if available
+  const [employeeCount, setEmployeeCount] = useState(
+    Number(localStorage.getItem('employeeCount')) || 0
+  );
+  const [averageServiceYears, setAverageServiceYears] = useState(
+    localStorage.getItem('averageServiceYears') || ''
+  );
+  const [averageServiceMonths, setAverageServiceMonths] = useState(
+    localStorage.getItem('averageServiceMonths') || ''
+  );
+  const [averageWage, setAverageWage] = useState(
+    Number(localStorage.getItem('averageWage')) || 0
+  );
+
   const { employees } = useFetchEmployees();
 
-  const employeeCount = employees.length
-  const serviceDays = employees.reduce((acc, employee) => {
-    const currentDate = new Date();
-    const hireDate = new Date(employee.doh);
-    const days = differenceInDays(currentDate, hireDate);
-    return acc + days;
-  }, 0)
-  const averageDays = serviceDays / employeeCount
-  const avgYears = Math.floor(averageDays / 365.25);
-  const avgMonths = Math.floor((averageDays % 365.25) / 30.4375);
+  useEffect(() => {
+    if (employees.length > 0) {
+      const employeeCount = employees.length;
+      setEmployeeCount(employeeCount);
+
+      const serviceDays = employees.reduce((acc, employee) => {
+        const currentDate = new Date();
+        const hireDate = new Date(employee.doh);
+        const days = differenceInDays(currentDate, hireDate);
+        return acc + days;
+      }, 0);
+
+      const averageDays = serviceDays / employeeCount;
+      const avgYears = Math.floor(averageDays / 365.25);
+      const avgMonths = Math.floor((averageDays % 365.25) / 30.4375);
+      setAverageServiceYears(avgYears);
+      setAverageServiceMonths(avgMonths);
+
+      const hourlyWage = employees.reduce((acc, employee) => acc + parseFloat(employee.wage), 0);
+      const avgWage = hourlyWage / employeeCount;
+      setAverageWage(avgWage.toFixed(2)); // Keeping two decimal places for wage
+    }
+  }, [employees]); // Ensure effect runs when `employees` changes
+
+  // Effect to store state changes in localStorage
+  useEffect(() => {
+    localStorage.setItem('employeeCount', employeeCount.toString());
+    localStorage.setItem('averageServiceYears', averageServiceYears);
+    localStorage.setItem('averageServiceMonths', averageServiceMonths);
+    localStorage.setItem('averageWage', averageWage.toString());
+  }, [employeeCount, averageServiceYears, averageServiceMonths, averageWage]); // Dependencies array includes the state variables to watch
 
   return (
-    <div>
+    <div className='home'>
       <h1>Welcome to Centric</h1>
       <h1>Fast Facts</h1>
+      <button onClick={() => console.log(averageServiceMonths)}>console</button>
       <ul>
-        <FactModule header='Employees' content={employeeCount} />
-        <FactModule header='Average Service Time' content={`${avgYears} years, ${avgMonths} months`} />
+        <FactModule header='Employees' numberOne={employeeCount} />
+        <FactModule header='Average Service Time' numberOne={averageServiceYears} numberTwo={averageServiceMonths} content='years' />
+        <FactModule header='Average Hourly Wage' numberOne={averageWage} />
       </ul>
     </div>
-  )
+  );
 }
